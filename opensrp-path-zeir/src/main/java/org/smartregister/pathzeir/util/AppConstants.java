474e8a7ab54af632b7c40b1f582ba2187cdb9a59
Hamza Ahmed Khan <hamza.khan@venturedive.com>
2021-03-17 15:25:37 +0500

    Include empty labels in registration data

--- opensrp-path-zeir/src/main/java/org/smartregister/pathzeir/util/AppConstants.java
+++ opensrp-path-zeir/src/main/java/org/smartregister/pathzeir/util/AppConstants.java
@@ -61,6 +61,7 @@ public class AppConstants {
         public static final String CHW_PHONE_NUMBER = "chw_phone_number";
         public static final String RESIDENTIAL_AREA = "residential_area";
         public static final String CHILD_ZONE = "child_zone";
+        public static final String BIRTH_WEIGHT = "Birth_Weight";
     }
 
     public static class ConfigurationConstants {
